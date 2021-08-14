9eee5885015e6c13eafbd19703775efb8d765c54
Nastya <anastasiiabashahurova@gmail.com>
2021-08-14 19:30:55 +0200

    HT-3

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,45 @@
-(function() {
-  var burger = document.querySelector(".burger");
-  var menu = document.querySelector("#" + burger.dataset.target);
-  burger.addEventListener("click", function() {
-    burger.classList.toggle("is-active");
-    menu.classList.toggle("is-active");
+(function () {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#' + burger.dataset.target);
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
+document.querySelectorAll('#nav li').forEach(function (navEl) {
+  navEl.onclick = function () {
     toggleTab(this.id, this.dataset.target);
   };
 });
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
+const btn = document.querySelector('#trigger');
+btn.addEventListener('click', triggerAlert);
 
-  navEls.forEach(function(navEl) {
+function triggerAlert() {
+  var alertEl = document.querySelector('.alert');
+  alertEl.setAttribute('role', 'alert');
+}
+function toggleTab(selectedNav, targetId) {
+  var navEls = document.querySelectorAll('#nav li');
+
+  navEls.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
+      navEl.classList.add('is-active');
     } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+      if (navEl.classList.contains('is-active')) {
+        navEl.classList.remove('is-active');
       }
     }
   });
 
-  var tabs = document.querySelectorAll(".tab-pane");
+  var tabs = document.querySelectorAll('.tab-pane');
 
-  tabs.forEach(function(tab) {
+  tabs.forEach(function (tab) {
     if (tab.id == targetId) {
-      tab.style.display = "block";
+      tab.style.display = 'block';
     } else {
-      tab.style.display = "none";
+      tab.style.display = 'none';
     }
   });
 }
